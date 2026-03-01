93a69bdfb3bdaba4b6e0b8b234cff2297258b9e7
Praneethsai24 <spraneeth698@gmail.com>
2026-03-02 00:19:49 +0530

    First lesson

--- GPT/1.js
+++ GPT/1.js
@@ -1,1 +1,15 @@
-// Variable
+// Variable and data types
+
+// Let (value can change) and Const (value cannot be reassigned)
+
+//Data Types
+// Number, String, Boolean, Undefined, Null
+
+
+const name = "Pranee"
+let age = 20;
+
+console.log(name);
+console.log(typeof(name));
+console.log(age);
+console.log(typeof(age));
